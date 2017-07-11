4c1ded890c7f004e7a9e32d33c73a16be1a455c2
Gordon Pimblott <g.pimblott@kainos.com>
2017-07-11 11:35:18 +0100

    Added ability to display stories by status and label

--- routes/stories.js
+++ routes/stories.js
@@ -1,3 +1,4 @@
+var debug = require('debug')('rubbernecker:stories');
 var express = require('express');
 var router = express.Router();
 var storyFetcher = require('../lib/storyFetcher');
@@ -40,8 +41,8 @@ router.get("/bugs", function (req, res, next) {
 });
 
 router.get("/notassigned", function (req, res, next) {
+    const milestoneNames = res.app.get('milestoneLabels');
 
-    var milestoneNames = res.app.get('milestoneLabels');
     storyFetcher.getAllStoriesWithoutLabels(res, milestoneNames , res.app.get('defaultProjects'), function (error, stories) {
 
         if (error) {
@@ -58,8 +59,28 @@ router.get("/notassigned", function (req, res, next) {
     });
 });
 
+router.get('/:status/:label', function (req, res, next) {
+    const status = req.params[ "status" ];
+    const label = req.params[ "label" ];
+
+    debug("searching for %s , %s" , status , label);
+
+    storyFetcher.getAllStoriesWithStatusAndLabel(res, status, label, res.app.get('defaultProjects'), function (error, stories) {
+        if (error) {
+            res.render('damn', {
+                message: '┬──┬◡ﾉ(° -°ﾉ)',
+                status: error,
+                reason: "(╯°□°）╯︵ ┻━┻"
+            });
+
+        } else {
+            utils.renderStories(res, stories, "Stories with status '" + status+"' and label '" + label + "'");
+        }
+    });
+});
+
 router.get('/:status', function (req, res, next) {
-  var status = req.params[ "status" ];
+  const status = req.params[ "status" ];
 
   storyFetcher.getAllStoriesWithStatus(res, status, res.app.get('defaultProjects'), function (error, stories) {
     if (error) {
@@ -73,7 +94,6 @@ router.get('/:status', function (req, res, next) {
       utils.renderStories(res, stories, "Stories with status '" + status+"'");
     }
   });
-
 });
 
 
