6599390c8b3a9b6a6a1532f3e88423fa134af356
Gordon Pimblott <g.pimblott@kainos.com>
2017-09-07 15:02:26 +0100

    Added search for updated and created stories

--- routes/stories.js
+++ routes/stories.js
@@ -6,44 +6,87 @@ var utils = require('../lib/utils');
 
 router.get('/', function (req, res, next) {
 
-  storyFetcher.getAllStories(res, res.app.get('defaultProjects'), function (error, stories) {
+    storyFetcher.getAllStories(res, res.app.get('defaultProjects'), function (error, stories) {
 
-    if (error) {
-      res.render('damn', {
-        message: '┬──┬◡ﾉ(° -°ﾉ)',
-        status: error,
-        reason: "(╯°□°）╯︵ ┻━┻"
-      });
+        if (error) {
+            res.render('damn', {
+                message: '┬──┬◡ﾉ(° -°ﾉ)',
+                status: error,
+                reason: "(╯°□°）╯︵ ┻━┻"
+            });
 
-    } else {
-      utils.renderStories(res, stories, "All Stories");
-    }
-  })
+        } else {
+            utils.renderStories(res, stories, "All Stories");
+        }
+    })
 
 });
 
 router.get("/bugs", function (req, res, next) {
 
-  storyFetcher.getAllBugs(res, res.app.get('defaultProjects'), function (error, stories) {
+    storyFetcher.getAllBugs(res, res.app.get('defaultProjects'), function (error, stories) {
 
-    if (error) {
+        if (error) {
 
-      res.render('damn', {
-        message: '┬──┬◡ﾉ(° -°ﾉ)',
-        status: error,
-        reason: "(╯°□°）╯︵ ┻━┻"
-      });
+            res.render('damn', {
+                message: '┬──┬◡ﾉ(° -°ﾉ)',
+                status: error,
+                reason: "(╯°□°）╯︵ ┻━┻"
+            });
 
+        } else {
+            utils.renderStories(res, stories, "Bugs");
+        }
+    });
+});
+
+router.get("/since/:changetype/:days?", function (req, res, next) {
+    var changeType = req.params[ "changetype" ];
+    var days = req.params[ "days" ];
+
+
+    if (!days) {
+        days = 7;
     } else {
-      utils.renderStories(res, stories, "Bugs");
+        days = parseInt(days);
+        if (isNaN(days)) {
+            days=7;
+        }
     }
-  });
+
+    var queryDate = new Date();
+    queryDate.setDate(queryDate.getDate() - days);
+    queryDate.setMinutes(0);
+    queryDate.setHours(0);
+    queryDate.setSeconds(0);
+
+    var dateSince = queryDate.toISOString();
+
+    var renderFunction = function (error, stories) {
+
+        if (error) {
+            res.render('Oh dear', {
+                message: '┬──┬◡ﾉ(° -°ﾉ)',
+                status: error,
+                reason: "(╯°□°）╯︵ ┻━┻"
+            });
+
+        } else {
+            utils.renderStories(res, stories, "Stories " + changeType + " in last  " + days + " days");
+        }
+    };
+
+    if (changeType === 'updated') {
+        storyFetcher.getAllStoriesUpdatedSince(res, dateSince, res.app.get('defaultProjects'), renderFunction);
+    } else {
+        storyFetcher.getAllStoriesCreatedSince(res, dateSince, res.app.get('defaultProjects'), renderFunction);
+    }
 });
 
 router.get("/notassigned", function (req, res, next) {
     const milestoneNames = res.app.get('milestoneLabels');
 
-    storyFetcher.getAllStoriesWithoutLabels(res, milestoneNames , res.app.get('defaultProjects'), function (error, stories) {
+    storyFetcher.getAllStoriesWithoutLabels(res, milestoneNames, res.app.get('defaultProjects'), function (error, stories) {
 
         if (error) {
 
@@ -63,7 +106,7 @@ router.get('/:status/:label', function (req, res, next) {
     const status = req.params[ "status" ];
     const label = req.params[ "label" ];
 
-    debug("searching for %s , %s" , status , label);
+    debug("searching for %s , %s", status, label);
 
     storyFetcher.getAllStoriesWithStatusAndLabel(res, status, label, res.app.get('defaultProjects'), function (error, stories) {
         if (error) {
@@ -74,27 +117,26 @@ router.get('/:status/:label', function (req, res, next) {
             });
 
         } else {
-            utils.renderStories(res, stories, "Stories with status '" + status+"' and label '" + label + "'");
+            utils.renderStories(res, stories, "Stories with status '" + status + "' and label '" + label + "'");
         }
     });
 });
 
 router.get('/:status', function (req, res, next) {
-  const status = req.params[ "status" ];
+    const status = req.params[ "status" ];
 
-  storyFetcher.getAllStoriesWithStatus(res, status, res.app.get('defaultProjects'), function (error, stories) {
-    if (error) {
-      res.render('damn', {
-        message: '┬──┬◡ﾉ(° -°ﾉ)',
-        status: error,
-        reason: "(╯°□°）╯︵ ┻━┻"
-      });
+    storyFetcher.getAllStoriesWithStatus(res, status, res.app.get('defaultProjects'), function (error, stories) {
+        if (error) {
+            res.render('damn', {
+                message: '┬──┬◡ﾉ(° -°ﾉ)',
+                status: error,
+                reason: "(╯°□°）╯︵ ┻━┻"
+            });
 
-    } else {
-      utils.renderStories(res, stories, "Stories with status '" + status+"'");
-    }
-  });
+        } else {
+            utils.renderStories(res, stories, "Stories with status '" + status + "'");
+        }
+    });
 });
-
 
 module.exports = router;
